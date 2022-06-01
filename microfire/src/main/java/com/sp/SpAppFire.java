17aa0c85310b3f8644e0de42e43416be5de58ccb
Rom9281 <34047259+Rom9281@users.noreply.github.com>
2022-06-01 17:37:44 +0200

    First commit : vehicle + fire

---
+++ microfire/src/main/java/com/sp/SpAppFire.java
@@ -0,0 +1,12 @@
+package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAppFire {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpAppFire.class,args);
+	}
+}
---
+++ microvehicle/src/main/java/com/sp/model/Vehicle.java
@@ -0,0 +1,14 @@
+package com.sp.model;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="VEHICLE")
+public class Vehicle {
+	
+}
