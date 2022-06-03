22b13614ba59f9b300103b6b3f68ea68b04bfb07
Captain Portable <zaghouane.fares@gmail.com>
2022-06-03 10:31:06 +0200

    ajout Dependance

--- microfire/src/main/java/com/sp/rest/FireRestCrt.java
+++ microfire/src/main/java/com/sp/rest/FireRestCrt.java
@@ -1,9 +1,12 @@
 package com.sp.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sp.model.Fire;
 import com.sp.service.FireService;
 
 
@@ -15,9 +18,15 @@ public class FireRestCrt {
 	/**
 	 * 
 	 * */
-	@RequestMapping(method = RequestMethod.GET, value = "/start")
-	public void tryFire() {
-		System.out.println(fService.getAlldBFires());
+	@RequestMapping(method = RequestMethod.GET, value = "/fires")
+	public Fire[] getFire() {
+		return fService.getAlldBFires();
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/fire/{idFire}")
+	public Fire getFire(@PathVariable String idFire) {
+		return fService.getFireById( Integer.valueOf(idFire));
 	}
 	
 	
